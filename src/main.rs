db77b62c08b82655e3398e27750679f6a69cb0cb
Jake Witcher <jjwitcher09@gmail.com>
2020-05-25 21:38:40 -0400

    add example parser to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use rusty_parsec::*;
+
 fn main() {
-    println!("Hello, world!");
+    let p_hello = p_string(String::from("hello"));
+    let p_goodbye = p_string(String::from("goodbye"));
+
+    let p_goodbye_or_hello =
+        Combinator::new(p_goodbye).or(p_hello).get_parser();
+
+    let p_comma = p_char(',');
+    let p_world = p_string(String::from("world"));
+
+    let hello_world =
+        Combinator::new(p_goodbye_or_hello)
+            .take_prev(p_comma)
+            .take_prev(ws())
+            .and(p_world)
+            .run(String::from("hello, world"));
+
+    match hello_world {
+        Ok((hello, world)) => 
+            println!("successfully parsed '{}' and '{}'", hello, world),
+        _ =>
+            println!("Failed to parse string 'hello, world'"),
+    }
 }
