b522281e3ae453adec19278902a78ee886e0e37e
Jake Witcher <jjwitcher09@gmail.com>
2020-06-08 21:14:48 -0400

    add to integration_tests, test for list of json objects and for nested objects and lists

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -118,33 +118,165 @@ fn p_key() -> Parser<String> {
 }
 
 #[test]
-fn suceeds_parsing_simple_json_object() {
+fn succeeds_parsing_simple_json_object() {
     let mut key_value_map = HashMap::new();
 
     key_value_map.insert("account active".to_string(), Json::JBool(true));
     key_value_map.insert("name".to_string(), Json::JString("Bob".to_string()));
     key_value_map.insert("age".to_string(), Json::JNumber(27.0));
 
-    let expected = Ok(ParserSuccess::new(Json::JObject(key_value_map), Position::new(1, 50, 49)));
+    let expected = Ok(ParserSuccess::new(Json::JObject(key_value_map), Position::new(5, 10, 97)));
 
-    let actual = p_json().run("{\"name\":\"Bob\", \"age\": 27, \"account active\": true}".to_string());
+    let actual = p_json().run(
+        "{
+            \"name\":\"Bob\", 
+            \"age\": 27, 
+            \"account active\": true
+        }".to_string());
 
     assert_eq!(expected, actual);
 }
 
 #[test]
-fn suceeds_parsing_json_object_with_list() {
+fn succeeds_parsing_json_object_with_list() {
     let mut key_value_map = HashMap::new();
     key_value_map.insert("account active".to_string(), Json::JBool(true));
     key_value_map.insert("name".to_string(), Json::JString("Bob".to_string()));
     key_value_map.insert("age".to_string(), Json::JNumber(27.0));
     key_value_map.insert("favorite numbers".to_string(), Json::JList(vec![Json::JNumber(1.0), Json::JNumber(2.0), Json::JNumber(3.0), Json::JNumber(4.0)]));
 
-    let expected = Ok(ParserSuccess::new(Json::JObject(key_value_map), Position::new(6, 2, 84)));
+    let expected = Ok(ParserSuccess::new(Json::JObject(key_value_map), Position::new(11, 10, 221)));
 
     let actual = p_json().run(
-        "{\n\"name\":\"Bob\",\n\"age\": 27,\n\"account active\": true,\n\"favorite numbers\":[1, 2, 3, 4]\n}".to_string()
+        "{
+            \"name\":\"Bob\",
+            \"age\": 27,
+            \"account active\": true,
+            \"favorite numbers\":[
+                1, 
+                2, 
+                3, 
+                4
+            ]
+        }".to_string()
     );
 
     assert_eq!(expected, actual);
+}
+
+#[test]
+fn succeed_parsing_list_of_json_objects() {
+    let person1 = create_person("Bob".to_string(), 35.0);
+    let person2 = create_person("Alex".to_string(), 48.0);
+    let person3 = create_person("Sarah".to_string(), 23.0);
+
+    let expected = Ok(ParserSuccess::new(Json::JList(vec![person1, person2, person3]), Position::new(12, 10, 253)));
+
+    let actual = p_json().run(
+        "[
+            { 
+                \"name\":\"Bob\", 
+                \"age\": 35 
+            }, { 
+                \"name\":\"Alex\", 
+                \"age\": 48 
+            }, { 
+                \"name\":\"Sarah\", 
+                \"age\": 23 
+            }
+        ]".to_string()
+    );
+
+    assert_eq!(expected, actual);
+}
+
+fn create_person(name: String, age: f64) -> Json {
+    let mut person = HashMap::new();
+    person.insert("name".to_string(), Json::JString(name));
+    person.insert("age".to_string(), Json::JNumber(age));
+
+    Json::JObject(person)
+}
+
+#[test]
+fn suceeds_parsing_nested_json_objects_and_lists() {
+    let person1 = create_person_with_address("Bob".to_string(), 35.0, 1008.0, "Main St.".to_string());
+    let person2 = create_person_with_address("Alex".to_string(), 48.0, 520.0, "Elm St.".to_string());
+    let person3 = create_person_with_address("Sarah".to_string(), 23.0, 1300.0, "Vine St.".to_string());
+
+    let mut obj = HashMap::new();
+    obj.insert("persons".to_string(), Json::JList(vec![person1, person2, person3]));
+
+    let expected = Ok(ParserSuccess::new(Json::JObject(obj), Position::new(41, 10, 1227)));
+
+    let actual = p_json().run(
+        "{
+            \"persons\": [
+                { 
+                    \"name\":\"Bob\", 
+                    \"age\": 35,
+                    \"address\": {
+                        \"number\": 1008,
+                        \"street\": \"Main St.\"
+                    },
+                    \"primary colors\": [
+                        \"red\",
+                        \"blue\",
+                        \"yellow\"
+                    ]
+                }, { 
+                    \"name\":\"Alex\", 
+                    \"age\": 48,
+                    \"address\": {
+                        \"number\": 520,
+                        \"street\": \"Elm St.\"
+                    },
+                    \"primary colors\": [
+                        \"red\",
+                        \"blue\",
+                        \"yellow\"
+                    ]
+                }, { 
+                    \"name\":\"Sarah\", 
+                    \"age\": 23,
+                    \"address\": {
+                        \"number\": 1300,
+                        \"street\": \"Vine St.\"
+                    },
+                    \"primary colors\": [
+                        \"red\",
+                        \"blue\",
+                        \"yellow\"
+                    ]
+                }
+            ]
+        }".to_string()
+    );
+
+    assert_eq!(expected, actual);
+}
+
+fn create_person_with_address(name: String, age: f64, number: f64, street:String) -> Json {
+    let mut person = HashMap::new();
+    person.insert("name".to_string(), Json::JString(name));
+    person.insert("age".to_string(), Json::JNumber(age));
+    person.insert("address".to_string(), create_address(number, street));
+    person.insert(
+        "primary colors".to_string(), 
+        Json::JList(vec![
+            Json::JString("red".to_string()), 
+            Json::JString("blue".to_string()), 
+            Json::JString("yellow".to_string())
+        ])
+    );
+
+    Json::JObject(person)
+}
+
+fn create_address(number: f64, street: String) -> Json {
+    let mut address = HashMap::new();
+    address.insert("number".to_string(), Json::JNumber(number));
+    address.insert("street".to_string(), Json::JString(street));
+
+    Json::JObject(address)
 }
