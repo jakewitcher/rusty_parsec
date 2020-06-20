aed088aebc726bc85c16fab794ff41c88d3e5eb2
Jake Witcher <jjwitcher09@gmail.com>
2020-06-20 19:34:49 -0400

    add skip_many_till parser and tests

--- src/parser/combinators.rs
+++ src/parser/combinators.rs
@@ -277,7 +277,6 @@ pub fn many_till<T, U>(parser: fn() -> Parser<T>, end_parser: fn() -> Parser<U>)
                                     results.push(success.get_result());
                                 }
                             }
-                            
                         },
                         Err(failure) => {
                             if results.len() == 0 {
@@ -321,7 +320,6 @@ pub fn many_1_till<T, U>(parser: fn() -> Parser<T>, end_parser: fn() -> Parser<U
                                     results.push(success.get_result());
                                 }
                             }
-                            
                         },
                         Err(failure) => {
                             let err = if results.len() == 0 {
@@ -336,6 +334,47 @@ pub fn many_1_till<T, U>(parser: fn() -> Parser<T>, end_parser: fn() -> Parser<U
                 }
 
                 Ok(ParserSuccess::new(results, state.get_position()))
+            }
+        );
+
+    Parser::new(parser_fn)
+}
+
+pub fn skip_many_till<T, U>(parser: fn() -> Parser<T>, end_parser: fn() -> Parser<U>) -> Parser<()> {
+    let parser_fn =
+        Box::new(
+            move |state: &mut ParserState| {
+                let mut end_parser_succeeds = false;
+                let mut state_changed = false;
+
+                while !end_parser_succeeds {
+                    match parser().parse(state) {
+                        Ok(_) => {
+                            state_changed = true;
+
+                            match end_parser().parse(state) {
+                                Ok(_) => {
+                                    end_parser_succeeds = true;
+                                },
+                                Err(failure) => {
+                                    if failure.is_fatal() {
+                                        return Err(failure)
+                                    }
+                                }
+                            }
+                        },
+                        Err(failure) => {
+                            if !state_changed {
+                                return Ok(ParserSuccess::new((), state.get_position()))
+                            } else
+                            {
+                                return Err(failure.to_fatal_err())
+                            }
+                        },
+                    }
+                }
+
+                Ok(ParserSuccess::new((), state.get_position()))
             }
         );
 
