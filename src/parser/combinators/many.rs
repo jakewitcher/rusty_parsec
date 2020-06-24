8e7a14b1c782f6efa93d47671769c64082eef997
Jake Witcher <jjwitcher09@gmail.com>
2020-06-23 22:28:36 -0400

    change get_parser_result to apply_parser in pipe.rs and many.rs, refactor many.rs to use match expression instead of if/else

--- src/parser/combinators/many.rs
+++ src/parser/combinators/many.rs
@@ -4,7 +4,7 @@ pub fn many<T>(parser: fn() -> Parser<T>) -> Parser<Vec<T>> {
     let parser_fn =
         Box::new(
             move |state: &mut ParserState| {
-                let results: Vec<T> = get_parser_results(parser, state);
+                let results: Vec<T> = apply_parser(parser, state);
                 Ok(ParserSuccess::new(results, state.get_position()))
             }
         );
@@ -16,16 +16,17 @@ pub fn many_1<T>(parser: fn() -> Parser<T>) -> Parser<Vec<T>> {
     let parser_fn =
         Box::new(
             move |state: &mut ParserState| {
-                let results: Vec<T> = get_parser_results(parser, state);
+                let results: Vec<T> = apply_parser(parser, state);
 
-                if results.len() == 0 {
-                    Err(ParserFailure::new_err(
-                        "value satisfying parser at least once".to_string(),
-                        None,
-                        state.get_position()
-                    ))
-                } else {
-                    Ok(ParserSuccess::new(results, state.get_position()))
+                match results.len() {
+                    0 => 
+                        Err(ParserFailure::new_err(
+                            "value satisfying parser at least once".to_string(),
+                            None,
+                            state.get_position()
+                        )),
+                    _ => 
+                        Ok(ParserSuccess::new(results, state.get_position()))
                 }
             }
         );
@@ -49,23 +50,26 @@ pub fn skip_many_1<T>(parser: fn() -> Parser<T>) -> Parser<()> {
     let parser_fn =
         Box::new(
             move |state: &mut ParserState| {
-                if let Ok(_) = parser().parse(state) {
-                    apply_parser(parser, state);
-                    return Ok(ParserSuccess::new((), state.get_position()))
+                match parser().parse(state) {
+                    Ok(_) => {
+                        apply_parser(parser, state);
+                        Ok(ParserSuccess::new((), state.get_position()))
+                    },
+                    _ => {
+                        Err(ParserFailure::new_err(
+                            "value satisfying parser at least once".to_string(),
+                            None,
+                            state.get_position()
+                        ))
+                    },
                 }
-                
-                Err(ParserFailure::new_err(
-                    "value satisfying parser at least once".to_string(),
-                    None,
-                    state.get_position()
-                ))
             }
         );
 
     Parser::new(parser_fn)
 }
 
-fn get_parser_results<T>(p: fn() -> Parser<T>, state: &mut ParserState) -> Vec<T> {
+fn apply_parser<T>(p: fn() -> Parser<T>, state: &mut ParserState) -> Vec<T> {
     let mut results: Vec<T> = Vec::new();
     let mut parser_succeeds = true;
 
@@ -81,8 +85,4 @@ fn get_parser_results<T>(p: fn() -> Parser<T>, state: &mut ParserState) -> Vec<T
     }
 
     results
-}
-
-fn apply_parser<T>(p: fn() -> Parser<T>, state: &mut ParserState) {
-    get_parser_results(p, state);
 }
