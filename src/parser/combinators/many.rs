e17e5be288c3f573672b6291602a75dfa0d55df8
Jake Witcher <jjwitcher09@gmail.com>
2020-06-23 22:17:08 -0400

    refactor many.rs

--- src/parser/combinators/many.rs
+++ src/parser/combinators/many.rs
@@ -4,20 +4,7 @@ pub fn many<T>(parser: fn() -> Parser<T>) -> Parser<Vec<T>> {
     let parser_fn =
         Box::new(
             move |state: &mut ParserState| {
-                let mut results: Vec<T> = Vec::new();
-                let mut parser_succeeds = true;
-
-                while parser_succeeds {
-                    match parser().parse(state) {
-                        Ok(success) => {
-                            results.push(success.get_result());
-                        },
-                        Err(_) => {
-                            parser_succeeds = false;
-                        },
-                    }
-                }
-
+                let results: Vec<T> = get_parser_results(parser, state);
                 Ok(ParserSuccess::new(results, state.get_position()))
             }
         );
@@ -29,19 +16,7 @@ pub fn many_1<T>(parser: fn() -> Parser<T>) -> Parser<Vec<T>> {
     let parser_fn =
         Box::new(
             move |state: &mut ParserState| {
-                let mut results: Vec<T> = Vec::new();
-                let mut parser_succeeds = true;
-
-                while parser_succeeds {
-                    match parser().parse(state) {
-                        Ok(success) => {
-                            results.push(success.get_result());
-                        },
-                        Err(_) => {
-                            parser_succeeds = false;
-                        },
-                    }
-                }
+                let results: Vec<T> = get_parser_results(parser, state);
 
                 if results.len() == 0 {
                     Err(ParserFailure::new_err(
@@ -62,19 +37,7 @@ pub fn skip_many<T>(parser: fn() -> Parser<T>) -> Parser<()> {
     let parser_fn =
         Box::new(
             move |state: &mut ParserState| {
-                let mut parser_succeeds = true;
-
-                while parser_succeeds {
-                    match parser().parse(state) {
-                        Ok(_) => {
-                            continue;
-                        },
-                        Err(_) => {
-                            parser_succeeds = false;
-                        },
-                    }
-                }
-
+                apply_parser(parser, state);
                 Ok(ParserSuccess::new((), state.get_position()))
             }
         );
@@ -87,19 +50,7 @@ pub fn skip_many_1<T>(parser: fn() -> Parser<T>) -> Parser<()> {
         Box::new(
             move |state: &mut ParserState| {
                 if let Ok(_) = parser().parse(state) {
-                    let mut parser_succeeds = true;
-
-                    while parser_succeeds {
-                        match parser().parse(state) {
-                            Ok(_) => {
-                                continue;
-                            },
-                            Err(_) => {
-                                parser_succeeds = false;
-                            },
-                        }
-                    }
-
+                    apply_parser(parser, state);
                     return Ok(ParserSuccess::new((), state.get_position()))
                 }
                 
@@ -112,4 +63,26 @@ pub fn skip_many_1<T>(parser: fn() -> Parser<T>) -> Parser<()> {
         );
 
     Parser::new(parser_fn)
+}
+
+fn get_parser_results<T>(p: fn() -> Parser<T>, state: &mut ParserState) -> Vec<T> {
+    let mut results: Vec<T> = Vec::new();
+    let mut parser_succeeds = true;
+
+    while parser_succeeds {
+        match p().parse(state) {
+            Ok(success) => {
+                results.push(success.get_result());
+            },
+            Err(_) => {
+                parser_succeeds = false;
+            },
+        }
+    }
+
+    results
+}
+
+fn apply_parser<T>(p: fn() -> Parser<T>, state: &mut ParserState) {
+    get_parser_results(p, state);
 }
