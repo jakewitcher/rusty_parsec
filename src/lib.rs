c0c19bbd5408646ecf0d7ef54cdf370111b36213
Jake Witcher <jjwitcher09@gmail.com>
2020-06-24 22:24:01 -0400

    add documentation for many parser

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,5 @@ mod parser;
 
 pub use parser::{ParserFn, Position, ParserState, ParserSuccess, ParserFailure, ParserResult, Parser};
 pub use parser::char_parsers::*;
-pub use parser::combinators::*;
+pub use parser::combinators::{choice, choice_l, attempt};
 pub use parser::combinators::{many::*, sep_by::*, many_till::*, pipe::*};
--- src/parser/combinators/many.rs
+++ src/parser/combinators/many.rs
@@ -1,10 +1,31 @@
 use super::{ParserState, ParserSuccess, ParserFailure, Parser};
 
-pub fn many<T>(parser: fn() -> Parser<T>) -> Parser<Vec<T>> {
+/// ```many``` applies the parser returned by ```p_factory``` repeatedly until it fails. A Vector of the successfully parsed values is returned. The parser must fail without changing the parser state or ```many``` will return a fatal error.
+/// 
+/// # Examples
+/// 
+/// ```
+/// use rusty_parsec::*;
+/// 
+/// fn p_hello() -> Parser<String> {
+///     p_string("hello".to_string())
+/// }
+/// 
+/// let expected = 
+///     Ok(ParserSuccess::new(
+///         vec![ "hello".to_string(), "hello".to_string(), "hello".to_string()], 
+///         Position::new(1, 16, 15))
+///     );
+/// 
+/// let actual = many(p_hello).run("hellohellohello".to_string());
+/// 
+/// assert_eq!(expected, actual);
+/// ```
+pub fn many<T>(p_factory: fn() -> Parser<T>) -> Parser<Vec<T>> {
     let parser_fn =
         Box::new(
             move |state: &mut ParserState| {
-                let results: Vec<T> = apply_parser(parser, state)?;
+                let results: Vec<T> = apply_parser(p_factory, state)?;
                 Ok(ParserSuccess::new(results, state.get_position()))
             }
         );
@@ -12,13 +33,13 @@ pub fn many<T>(parser: fn() -> Parser<T>) -> Parser<Vec<T>> {
     Parser::new(parser_fn)
 }
 
-pub fn many_1<T>(parser: fn() -> Parser<T>) -> Parser<Vec<T>> {
+pub fn many_1<T>(p_factory: fn() -> Parser<T>) -> Parser<Vec<T>> {
     let parser_fn =
         Box::new(
             move |state: &mut ParserState| {
-                match parser().parse(state) {
+                match p_factory().parse(state) {
                     Ok(success) => {
-                        let mut results = apply_parser(parser, state)?;
+                        let mut results = apply_parser(p_factory, state)?;
                         results.insert(0, success.get_result());
                         Ok(ParserSuccess::new(results, state.get_position()))
                     },
@@ -30,11 +51,11 @@ pub fn many_1<T>(parser: fn() -> Parser<T>) -> Parser<Vec<T>> {
     Parser::new(parser_fn)
 }
 
-pub fn skip_many<T>(parser: fn() -> Parser<T>) -> Parser<()> {
+pub fn skip_many<T>(p_factory: fn() -> Parser<T>) -> Parser<()> {
     let parser_fn =
         Box::new(
             move |state: &mut ParserState| {
-                let _ = apply_parser(parser, state)?;
+                let _ = apply_parser(p_factory, state)?;
                 Ok(ParserSuccess::new((), state.get_position()))
             }
         );
@@ -42,13 +63,13 @@ pub fn skip_many<T>(parser: fn() -> Parser<T>) -> Parser<()> {
     Parser::new(parser_fn)
 }
 
-pub fn skip_many_1<T>(parser: fn() -> Parser<T>) -> Parser<()> {
+pub fn skip_many_1<T>(p_factory: fn() -> Parser<T>) -> Parser<()> {
     let parser_fn =
         Box::new(
             move |state: &mut ParserState| {
-                match parser().parse(state) {
+                match p_factory().parse(state) {
                     Ok(_) => {
-                        let _ = apply_parser(parser, state)?;
+                        let _ = apply_parser(p_factory, state)?;
                         Ok(ParserSuccess::new((), state.get_position()))
                     },
                     Err(failure) => Err(failure),
