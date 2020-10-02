b44c4a29855ecf06cee74dc580645345710b0edd
Jake Witcher <jjwitcher09@gmail.com>
2020-10-01 20:47:28 -0400

    format sep_by_tests

--- tests/combinators_sep_by_tests.rs
+++ tests/combinators_sep_by_tests.rs
@@ -2,117 +2,139 @@ use rusty_parsec::*;
 
 #[test]
 fn succeeds_parsing_with_sep_by() {
-    let expected = Ok(ParserSuccess::new(vec![1, 2, 3], Position::new(1, 6, 5)));
+    let expected = Ok(ParserSuccess::new(
+        vec![1, 2, 3], 
+        Position::new(1, 6, 5)
+    ));
 
-    let actual =
-        sep_by(
-            p_u32,
-            || p_char(';')
-        ).run("1;2;3".to_string());
+    let actual = sep_by(
+        p_u32,
+        || p_char(';')
+    ).run(String::from("1;2;3"));
 
-    assert_eq!(expected, actual);
+    assert_eq!(actual, expected);
 }
 
 #[test]
 fn succeeds_parsing_with_sep_by_returns_empty_vec() {
-    let expected = Ok(ParserSuccess::new(Vec::new(), Position::new(1, 1, 0)));
+    let expected = Ok(ParserSuccess::new(
+        Vec::new(), 
+        Position::new(1, 1, 0)
+    ));
 
-    let actual =
-        sep_by(
-            p_u32,
-            || p_char(';')
-        ).run("a;b;c".to_string());
+    let actual = sep_by(
+        p_u32,
+        || p_char(';')
+    ).run(String::from("a;b;c"));
 
-    assert_eq!(expected, actual);
+    assert_eq!(actual, expected);
 }
 
 #[test]
 fn fails_parsing_with_sep_by() {
-    let expected = Err(ParserFailure::new_fatal_err("A".to_string(), Some("a".to_string()), Position::new(1, 2, 1)));
+    let expected = Err(ParserFailure::new_fatal_err(
+        String::from("A"), 
+        Some(String::from("a")), 
+        Position::new(1, 2, 1)
+    ));
 
-    let actual =
-        sep_by(
-            || p_u32().and(p_char('A')),
-            || p_char(';')
-        ).run("1a;2b;3c".to_string());
+    let actual = sep_by(
+        || p_u32().and(p_char('A')),
+        || p_char(';')
+    ).run(String::from("1a;2b;3c"));
 
-    assert_eq!(expected, actual);
+    assert_eq!(actual, expected);
 }
 
 #[test]
 fn fails_parsing_separator_with_sep_by() {
-    let expected = Err(ParserFailure::new_fatal_err(">".to_string(), Some("?".to_string()), Position::new(1, 6, 5)));
+    let expected = Err(ParserFailure::new_fatal_err(
+        String::from(">"), 
+        Some(String::from("?")), 
+        Position::new(1, 6, 5)
+    ));
 
-    let actual =
-        sep_by(
-            p_u32,
-            || p_char('<').and(p_char('>'))
-        ).run("1<>2<?3".to_string());
+    let actual = sep_by(
+        p_u32,
+        || p_char('<').and(p_char('>'))
+    ).run(String::from("1<>2<?3"));
 
-    assert_eq!(expected, actual);
+    assert_eq!(actual, expected);
 }
 
 #[test]
 fn succeeds_parsing_with_sep_by_1() {
-    let expected = Ok(ParserSuccess::new(vec![1, 2, 3], Position::new(1, 6, 5)));
+    let expected = Ok(ParserSuccess::new(
+        vec![1, 2, 3], 
+        Position::new(1, 6, 5)
+    ));
 
-    let actual =
-        sep_by_1(
-            p_u32,
-            || p_char(';')
-        ).run("1;2;3".to_string());
+    let actual = sep_by_1(
+        p_u32,
+        || p_char(';')
+    ).run(String::from("1;2;3"));
 
-    assert_eq!(expected, actual);
+    assert_eq!(actual, expected);
 }
 
 #[test]
 fn fails_parsing_with_sep_by_1() {
-    let expected = Err(ParserFailure::new_err("value satisfying parser at least once".to_string(), None, Position::new(1, 1, 0)));
+    let expected = Err(ParserFailure::new_err(
+        String::from("value satisfying parser at least once"), 
+        None, 
+        Position::new(1, 1, 0)
+    ));
 
-    let actual =
-        sep_by_1(
-            p_u32,
-            || p_char(';')
-        ).run("a;b;c".to_string());
+    let actual = sep_by_1(
+        p_u32,
+        || p_char(';')
+    ).run(String::from("a;b;c"));
 
-    assert_eq!(expected, actual);
+    assert_eq!(actual, expected);
 }
 
 #[test]
 fn succeeds_parsing_with_skip_sep_by() {
-    let expected = Ok(ParserSuccess::new((), Position::new(1, 6, 5)));
+    let expected = Ok(ParserSuccess::new(
+        (), 
+        Position::new(1, 6, 5)
+    ));
 
-    let actual =
-        skip_sep_by(
-            p_u32,
-            || p_char(';')
-        ).run("1;2;3".to_string());
+    let actual = skip_sep_by(
+        p_u32,
+        || p_char(';')
+    ).run(String::from("1;2;3"));
 
-    assert_eq!(expected, actual);
+    assert_eq!(actual, expected);
 }
 
 #[test]
 fn succeeds_parsing_with_skip_sep_by_1() {
-    let expected = Ok(ParserSuccess::new((), Position::new(1, 6, 5)));
+    let expected = Ok(ParserSuccess::new(
+        (), 
+        Position::new(1, 6, 5)
+    ));
 
-    let actual =
-        skip_sep_by_1(
-            p_u32,
-            || p_char(';')
-        ).run("1;2;3".to_string());
+    let actual = skip_sep_by_1(
+        p_u32,
+        || p_char(';')
+    ).run(String::from("1;2;3"));
 
-    assert_eq!(expected, actual);
+    assert_eq!(actual, expected);
 }
 
 #[test]
 fn fails_parsing_with_skip_sep_by_1() {
-    let expected = Err(ParserFailure::new_err("value satisfying parser at least once".to_string(), None, Position::new(1, 1, 0)));
+    let expected = Err(ParserFailure::new_err(
+        String::from("value satisfying parser at least once"), 
+        None, 
+        Position::new(1, 1, 0)
+    ));
 
-    let actual =
-        skip_sep_by_1(
-            p_u32,
-            || p_char(';')
-        ).run("a;b;c".to_string());
+    let actual = skip_sep_by_1(
+        p_u32,
+        || p_char(';')
+    ).run(String::from("a;b;c"));
 
-    assert_eq!(expected, actual);
+    assert_eq!(actual, expected);
 }
