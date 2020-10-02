b1c742ede2c49c5da1b3338cce075e736726f8b8
Jake Witcher <jjwitcher09@gmail.com>
2020-10-01 21:08:56 -0400

    update documentation for sep_by

--- src/parser/combinators/sep_by.rs
+++ src/parser/combinators/sep_by.rs
@@ -1,25 +1,32 @@
 use super::{ParserState, ParserSuccess, ParserFailure, Parser};
 
-/// ```sep_by``` applies the parser returned by ```get_parser``` followed by the parser returned by ```get_separator``` repeatedly until either parser fails. A Vector of the parsed values is returned once the parser fails. The parsers must fail without changing the parser state or ```sep_by``` will return a fatal error.
+/// `sep_by` takes two parsers and applies the first parser (`parser`) followed by the second parser (`separator`) repeatedly until one of them fails.
+/// Once either parser fails, all values parsed by the `parser` are returned in a Vector as a `ParserSuccess`.
+/// If the `parser` fails on the first attempt, `sep_by` will return a `ParserSuccess` with an empty Vector.
+/// 
+/// # Errors
+/// `sep_by` will return a `ParserFailure` with a `FatalError` if either the `parser` or the `separator` fails having changed the parser state.
 /// 
 /// # Examples
 /// 
 /// ```
-/// use rusty_parsec::*;
+/// # use rusty_parsec::*;
+/// #
+/// # fn p_comma() -> Parser<char> {
+/// #     p_char(',')
+/// # }
+/// #
+/// let expected = Ok(ParserSuccess::new(
+///     vec![1,2,3], 
+///     Position::new(1, 6, 5))
+/// );
 /// 
-/// fn p_comma() -> Parser<char> {
-///     p_char(',')
-/// }
+/// let actual = sep_by(
+///     p_u32, 
+///     p_comma
+/// ).run(String::from("1,2,3"));
 /// 
-/// let expected = 
-///     Ok(ParserSuccess::new(
-///         vec![1,2,3], 
-///         Position::new(1, 6, 5))
-///     );
-/// 
-/// let actual = sep_by(p_u32, p_comma).run("1,2,3".to_string());
-/// 
-/// assert_eq!(expected, actual);
+/// assert_eq!(actual, expected);
 /// ```
 pub fn sep_by<T, U>(parser: fn() -> Parser<T>, separator: fn() -> Parser<U>) -> Parser<Vec<T>> 
 where U: 'static
@@ -35,27 +42,35 @@ where U: 'static
     Parser::new(parser_fn)
 }
 
-/// ```sep_by_1``` is similar to ```sep_by``` however ```sep_by_1``` must succeed at least once. A Vector of the parsed values is returned once the parser fails. The parsers must fail without changing the parser state or ```sep_by_1``` will return a fatal error.
+/// `sep_by_1` takes two parsers and applies the first parser (`parser`) followed by the second parser (`separator`) repeatedly until one of them fails.
+/// Once either parser fails, all values parsed by the `parser` are returned in a Vector as a `ParserSuccess`.
+/// 
+/// # Errors
+/// `sep_by_1` will return a `ParserFailure` with a `FatalError` if either the `parser` or the `separator` fails having changed the parser state.
+/// Unlinke `sep_by`, if the `parser` fails on the first attempt, `sep_by_1` will return a `ParserFailure`. The `parser` must succeed at least
+/// once for `sep_by_1` to return a `ParserSuccess`.
 /// 
 /// # Examples
 /// 
 /// ```
-/// use rusty_parsec::*;
+/// # use rusty_parsec::*;
+/// #
+/// # fn p_comma() -> Parser<char> {
+/// #     p_char(',')
+/// # }
+/// #
+/// let expected = Err(ParserFailure::new_err(
+///     String::from("value satisfying parser at least once"), 
+///     None,
+///     Position::new(1, 1, 0))
+/// );
 /// 
-/// fn p_comma() -> Parser<char> {
-///     p_char(',')
-/// }
+/// let actual = sep_by_1(
+///     p_u32, 
+///     p_comma
+/// ).run(String::from("A,B,C"));
 /// 
-/// let expected = 
-///     Err(ParserFailure::new_err(
-///         "value satisfying parser at least once".to_string(), 
-///         None,
-///         Position::new(1, 1, 0))
-///     );
-/// 
-/// let actual = sep_by_1(p_u32, p_comma).run("A,B,C".to_string());
-/// 
-/// assert_eq!(expected, actual);
+/// assert_eq!(actual, expected);
 /// ```
 pub fn sep_by_1<T, U>(parser: fn() -> Parser<T>, separator: fn() -> Parser<U>) -> Parser<Vec<T>> 
 where U: 'static
@@ -80,26 +95,33 @@ where U: 'static
     Parser::new(parser_fn)
 }
 
-/// ```skip_sep_by``` applies the parser returned by ```get_parser``` followed by the parser returned by ```get_separator``` repeatedly until either parser fails and returns ```()``` as the result. The parsers must fail without changing the parser state or ```skip_sep_by``` will return a fatal error.
+/// `skip_sep_by` takes two parsers and applies the first parser (`parser`) followed by the second parser (`separator`) repeatedly until one of them fails.
+/// Once either parser fails, `()` is returned as a `ParserSuccess`.
+/// If the `parser` fails on the first attempt, `skip_sep_by` will return a `ParserSuccess` of `()`.
+/// 
+/// # Errors
+/// `skip_sep_by` will return a `ParserFailure` with a `FatalError` if either the `parser` or the `separator` fails having changed the parser state.
 /// 
 /// # Examples
 /// 
 /// ```
-/// use rusty_parsec::*;
+/// # use rusty_parsec::*;
+/// #
+/// # fn p_comma() -> Parser<char> {
+/// #     p_char(',')
+/// # }
+/// #
+/// let expected = Ok(ParserSuccess::new(
+///     (), 
+///     Position::new(1, 6, 5))
+/// );
 /// 
-/// fn p_comma() -> Parser<char> {
-///     p_char(',')
-/// }
+/// let actual = skip_sep_by(
+///     p_u32, 
+///     p_comma
+/// ).run(String::from("1,2,3"));
 /// 
-/// let expected = 
-///     Ok(ParserSuccess::new(
-///         (), 
-///         Position::new(1, 6, 5))
-///     );
-/// 
-/// let actual = skip_sep_by(p_u32, p_comma).run("1,2,3".to_string());
-/// 
-/// assert_eq!(expected, actual);
+/// assert_eq!(actual, expected);
 /// ```
 pub fn skip_sep_by<T, U>(parser: fn() -> Parser<T>, separator: fn() -> Parser<U>) -> Parser<()> 
 where U: 'static
@@ -115,27 +137,35 @@ where U: 'static
     Parser::new(parser_fn)
 }
 
-/// ```skip_sep_by_1``` is similar to ```sep_by``` however ```skip_sep_by_1``` must succeed at least once before returning ```()```. The parsers must fail without changing the parser state or ```sep_by_1``` will return a fatal error.
+/// `skip_sep_by_1` takes two parsers and applies the first parser (`parser`) followed by the second parser (`separator`) repeatedly until one of them fails.
+/// Once either parser fails, `()` is returned as a `ParserSuccess`.
+/// 
+/// # Errors
+/// `skip_sep_by_1` will return a `ParserFailure` with a `FatalError` if either the `parser` or the `separator` fails having changed the parser state.
+/// Unlinke `sep_by`, if the `parser` fails on the first attempt, `skip_sep_by_1` will return a `ParserFailure`. The `parser` must succeed at least
+/// once for `skip_sep_by_1` to return a `ParserSuccess`.
 /// 
 /// # Examples
 /// 
 /// ```
-/// use rusty_parsec::*;
+/// # use rusty_parsec::*;
+/// #
+/// # fn p_comma() -> Parser<char> {
+/// #     p_char(',')
+/// # }
+/// #
+/// let expected = Err(ParserFailure::new_err(
+///     String::from("value satisfying parser at least once"), 
+///     None,
+///     Position::new(1, 1, 0))
+/// );
 /// 
-/// fn p_comma() -> Parser<char> {
-///     p_char(',')
-/// }
+/// let actual = skip_sep_by_1(
+///     p_u32, 
+///     p_comma
+/// ).run(String::from("A,B,C"));
 /// 
-/// let expected = 
-///     Err(ParserFailure::new_err(
-///         "value satisfying parser at least once".to_string(), 
-///         None,
-///         Position::new(1, 1, 0))
-///     );
-/// 
-/// let actual = skip_sep_by_1(p_u32, p_comma).run("A,B,C".to_string());
-/// 
-/// assert_eq!(expected, actual);
+/// assert_eq!(actual, expected);
 /// ```
 pub fn skip_sep_by_1<T, U>(parser: fn() -> Parser<T>, separator: fn() -> Parser<U>) -> Parser<()> 
 where U: 'static
